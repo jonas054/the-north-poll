5c6eb49998d0781b8f8c30228b6987bb53c90313
Jonas Arvidsson <jonas054@gmail.com>
2019-10-20 19:04:34 +0200

    Revert "Add render_async gem and use it in list view"
    This reverts commit 9d131f1d5cf5124d6c76cbd17460049272451ad7.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'poll/:id/results', controller: 'poll', action: 'results'
   get 'poll/create_linked/:id', controller: 'poll', action: 'create_linked'
   get 'poll/list/:id', controller: 'poll', action: 'list'
-  get 'poll/list_async/:id', controller: 'poll', action: 'list_async'
   get 'vote/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'vote#index'
