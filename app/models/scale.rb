d5c92efb24d8dbe7da0abae28442c5b5c85eda2c
Jonas Arvidsson <jonas054@gmail.com>
2019-10-20 10:20:02 +0200

    Add planning poker scale and refactor

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -1,2 +1,9 @@
+# coding: utf-8
 class Scale < ApplicationRecord
+  def self.default_lists
+    [%w[1 2 3 4 5 6 7 8 9 10],
+     %w[½ 1 2 3 5 8 13 20 40 100 ? ☕️],
+     %w[Yes No],
+     %w[👍 👎]]
+  end
 end
