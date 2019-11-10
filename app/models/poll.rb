9db68f63ef4219c125a3001c893bc27f97efe75e
Jonas Arvidsson <jonas054@gmail.com>
2019-11-10 15:42:43 +0100

    It's nil not null in Ruby

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -35,7 +35,7 @@ class Poll < ApplicationRecord
   end
 
   def remove_links_to
-    Poll.update(previous_poll_id, next_poll_id: null) if previous_poll_id
-    Poll.update(next_poll_id, previous_poll_id: null) if next_poll_id
+    Poll.update(previous_poll_id, next_poll_id: nil) if previous_poll_id
+    Poll.update(next_poll_id, previous_poll_id: nil) if next_poll_id
   end
 end
