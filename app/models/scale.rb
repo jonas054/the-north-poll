bd5347a7284d961f737603f9b150be19b3125b39
Jonas Arvidsson <jonas054@gmail.com>
2020-07-04 11:31:38 +0200

    Put coffee cup first in the series for planning poker

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -5,7 +5,7 @@ class Scale < ApplicationRecord
 
   def self.default_lists
     [%w[1 2 3 4 5 6 7 8 9 10],
-     %w[½ 1 2 3 5 8 13 20 40 100 ? ☕️],
+     %w[☕ ½ 1 2 3 5 8 13 20 40 100 ?️],
      %w[Yes No],
      %w[👍 🤷‍♂️ 👎]]
   end
