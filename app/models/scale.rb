61c452c31da77e99f34e523c823b8e216621ee14
Jonas Arvidsson <jonas054@gmail.com>
2020-07-05 12:02:17 +0200

    Add T-shirt sizes to default scales

--- app/models/scale.rb
+++ app/models/scale.rb
@@ -6,6 +6,7 @@ class Scale < ApplicationRecord
   def self.default_lists
     [%w[1 2 3 4 5 6 7 8 9 10],
      %w[☕ ½ 1 2 3 5 8 13 20 40 100 ?️],
+     %w[S M L XL],
      %w[Yes No],
      %w[👍 🤷‍♂️ 👎]]
   end
